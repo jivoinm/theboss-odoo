6483a926af86ef9db738984d0d13f4c7ce457f9b
Mirća Živojin <jivoinm@gmail.com>
2021-01-17 11:02:18 -0500

    Stock rule update

--- theboss_sale_mrp/models/mrp_production.py
+++ theboss_sale_mrp/models/mrp_production.py
@@ -15,15 +15,15 @@ class MrpProduction(models.Model):
 class StockRule(models.Model):
     _inherit = 'stock.rule'
 
-    # @api.model
-    # def _prepare_mo_vals(self, product_id, product_qty, product_uom, location_id, name, origin, company_id, values, bom):
-    #     _logger.info("Entering _prepare_mo_vals, {0}, origin:{1}, values:{2}, company_id:{3}".format(self, origin,values, company_id))
-    #     vals = super(StockRule, self)._prepare_mo_vals(product_id, product_qty, product_uom, location_id, name, origin, company_id, values, bom)
-    #     if "orderpoint_id" in values:
-    #         _logger.info("orderpoint_id={0}, group_id:{1}".format(values["orderpoint_id"].id, values["orderpoint_id"].group_id.id))
-    #         sales = self.env['sale.order'].search([('id', '=', values["orderpoint_id"].group_id.sale_order_id)])
-    #         _logger.info("found sale order {0}".format(sales.id))
-    #         if len(sales) > 1:
-    #                 raise exceptions.ValidationError(_('More than 1 sale order found for this group'))
-    #         vals['sale_order_id'] = sales.id
-    #     return vals
+    @api.model
+    def _prepare_mo_vals(self, product_id, product_qty, product_uom, location_id, name, origin, company_id, values, bom):
+        _logger.info("Entering _prepare_mo_vals, {0}, origin:{1}, values:{2}, company_id:{3}".format(self, origin,values, company_id))
+        vals = super(StockRule, self)._prepare_mo_vals(product_id, product_qty, product_uom, location_id, name, origin, company_id, values, bom)
+        if "orderpoint_id" in values:
+            _logger.info("orderpoint_id={0}, group_id:{1}".format(values["orderpoint_id"].id, values["orderpoint_id"].group_id.id))
+            sales = self.env['sale.order'].search([('id', '=', values["orderpoint_id"].group_id.sale_order_id)])
+            _logger.info("found sale order {0}".format(sales.id))
+            if len(sales) > 1:
+                    raise exceptions.ValidationError(_('More than 1 sale order found for this group'))
+            vals['sale_order_id'] = sales.id
+        return vals
